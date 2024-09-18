052390f9da5f0b4a9c23cbff3864e1141babc15c
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-09-19 02:04:24 +0400

    feat: project setup

---
+++ apps/app.module.ts
@@ -0,0 +1,10 @@
+import { DatabaseModule } from '@app/database';
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [DatabaseModule, UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
